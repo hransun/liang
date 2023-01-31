ea28ac5e4d1db46d9f106b35f1acae4916576573
Haoran Sun <haoran.sun@hpe.com>
2023-01-31 10:23:51 -0800

    0131

--- src/linkedlist/episode1/LinkMain.java
+++ src/linkedlist/episode1/LinkMain.java
@@ -25,30 +25,32 @@ public class LinkMain {
 
         System.out.println("original linked list");
         while (p != null) {
-            System.out.printf("%d\t",p.val);
+            System.out.printf("%d\t", p.val);
             p = p.next;
         }
+        System.out.println();
 
+        System.out.println("insert 10 to the end of list");
         // insert a node
-        head = insert(head,10);
+        head = insert(head, 10);
         p = head;
         while (p != null) {
-            System.out.printf("%d\t",p.val);
+            System.out.printf("%d\t", p.val);
             p = p.next;
         }
         System.out.println();
 
         // delete node "4"
-        head = delete(head,4);
+        head = delete(head, 4);
         p = head;
-        System.out.println("\n insert node 4 in linkedlist");
+        System.out.println("\n delete node.val == 4 in linkedlist");
         while (p != null) {
-            System.out.printf("%d\t",p.val);
+            System.out.printf("%d\t", p.val);
             p = p.next;
         }
         System.out.println();
 
-        LinkNode r = find(head,6);
+        LinkNode r = find(head, 6);
         if (r == null) {
             System.out.println(" not able to find 6th node");
         } else {
@@ -56,17 +58,89 @@ public class LinkMain {
         }
         System.out.println();
 
-        boolean ans = update(head,2,100);
+        boolean ans = update(head, 2, 100);
         p = head;
         System.out.println("update 2nd node is 100");
-        while (p != null){
-            System.out.printf("%d\t",p.val);
+        while (p != null) {
+            System.out.printf("%d\t", p.val);
             p = p.next;
         }
         System.out.println();
 
 
     }
+
+    // create a  list
+    private static LinkNode create() {
+        LinkNode head = new LinkNode(0, null);
+        LinkNode p = head;
+        for (int i = 1; i < 5; i++) {
+            p.next = new LinkNode(i * 2, null);
+            p = p.next;
+        }
+        return head;
+    }
+
+    // insert a node with val
+    private static LinkNode insert(LinkNode head, int val) {
+        LinkNode target = new LinkNode(val, null);
+        LinkNode p = head;
+        if (head == null || head.val >= val) {
+            target.next = head;
+            return target;
+        }
+        while (p.next != null && p.next.val < val) {
+            p = p.next;
+        }
+        // insert pos
+        target.next = p.next;
+        p.next = target;
+        return head;
+
+    }
+
+    //delete node with val
+    private static LinkNode delete(LinkNode head, int val) {
+        if (head == null) return head;
+        // deal with header corner case
+        if (head.val == val) {
+            LinkNode newHead = head.next;
+            head.next = null;
+            return newHead;
+        }
+
+        LinkNode p = head;
+        while (p.next != null && p.next.val != val) {
+            p = p.next;
+        }
+        // either p.next == null or p.next.val = val
+        if (p.next != null) {
+            LinkNode target = p.next;
+            p.next = target.next;
+            target.next = null;
+        }
+        return head;
+
+    }
+
+    // update kth node start with 1 , with val
+    private static boolean update(LinkNode head, int k, int val) {
+        if (head == null) return false;
+        int cnt = 1;
+        LinkNode p = head;
+        while(p.next !=null && cnt<k) {
+            p = p.next;
+            cnt++;
+        }
+        if  (cnt < k) {
+            return false;
+        }
+        p.val = val;
+        return true;
+    }
+
+
+
 
     // find kth node start from 1
     private static LinkNode find(LinkNode head, int k) {
@@ -85,6 +159,4 @@ public class LinkMain {
         return p ;
     }
 
-    private static LinkNode create() {
-    }
 }
