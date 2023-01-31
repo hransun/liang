104d4514138c0df516e4eb809c795e648862ad91
Haoran Sun <haoran.sun@hpe.com>
2023-01-30 21:01:33 -0800

    0130

--- src/linkedlist/episode1/ArrayMain.java
+++ src/linkedlist/episode1/ArrayMain.java
@@ -1,2 +1,67 @@
-package linkedlist.episode1;public class ArrayMain {
+package linkedlist.episode1;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        // top down design
+        // queue has 10 ppl
+        int[] qu = new int[10];// fixed length
+        qu[0]= 1;
+        qu[1]= 3;
+        qu[2]= 6;
+        qu[3]= 8;
+        qu[4]= 9;
+        int quLen = 5;
+        System.out.println("original qu:");
+        for (int i = 0; i < quLen; i++) {
+            System.out.printf("%d\t",qu[i]);
+        }
+        System.out.println();
+
+        // add element
+        int kid = 7;
+        quLen = enQueue(qu,quLen,kid);
+        System.out.println("new queue");
+        for (int i = 0; i < quLen; i++) {
+            System.out.printf("%d\t",qu[i]);
+        }
+        System.out.println();
+
+        // delete elements
+        quLen = leaveQueue(qu,quLen,2);
+        System.out.println("after dequeue");
+        for (int i = 0; i < quLen; i++) {
+            System.out.printf("%d\t",qu[i]);
+        }
+        System.out.println();
+
+
+        }
+
+    private static int leaveQueue(int[] qu, int quLen, int i) {
+        int kid = qu[i];
+        for (int j = i+1; j <quLen ; j++) {
+            qu[j-1] = qu[j];
+        }
+        return quLen-1;
+    }
+
+    public  static int enQueue(int[] qu, int quLen,int kid) {
+        int pos = quLen;
+        for (int i = 0; i < quLen; i++) {
+            if (qu[i] >=kid) {
+                pos = i;
+                break;
+            }
+        }
+        for (int i = quLen-1; i >=pos ; i--) {
+            qu[i+1] = qu[i];
+
+        }
+        qu[pos] = kid;
+        return quLen+1;
+
+
+    }
 }
--- src/linkedlist/episode1/LinkMain.java
+++ src/linkedlist/episode1/LinkMain.java
@@ -1,2 +1,90 @@
-package linkedlist.episode1;public class LinkMain {
+package linkedlist.episode1;
+
+import javax.swing.text.StyledEditorKit;
+
+/**
+ *  define linked list
+ */
+class LinkNode {
+    int val;
+    LinkNode next;
+    LinkNode() {
+        val = 0;
+        next = null;
+    }
+    LinkNode(int val , LinkNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class LinkMain {
+    public static void main(String[] args) {
+        LinkNode head = create();
+        LinkNode p = head;
+
+        System.out.println("original linked list");
+        while (p != null) {
+            System.out.printf("%d\t",p.val);
+            p = p.next;
+        }
+
+        // insert a node
+        head = insert(head,10);
+        p = head;
+        while (p != null) {
+            System.out.printf("%d\t",p.val);
+            p = p.next;
+        }
+        System.out.println();
+
+        // delete node "4"
+        head = delete(head,4);
+        p = head;
+        System.out.println("\n insert node 4 in linkedlist");
+        while (p != null) {
+            System.out.printf("%d\t",p.val);
+            p = p.next;
+        }
+        System.out.println();
+
+        LinkNode r = find(head,6);
+        if (r == null) {
+            System.out.println(" not able to find 6th node");
+        } else {
+            System.out.println("the 6th node is " + r.val);
+        }
+        System.out.println();
+
+        boolean ans = update(head,2,100);
+        p = head;
+        System.out.println("update 2nd node is 100");
+        while (p != null){
+            System.out.printf("%d\t",p.val);
+            p = p.next;
+        }
+        System.out.println();
+
+
+    }
+
+    // find kth node start from 1
+    private static LinkNode find(LinkNode head, int k) {
+        if (head == null) {
+            return null;
+        }
+        int cnt =1;
+        LinkNode p = head;
+        while (p.next != null && cnt < k) {
+            p = p.next;
+            cnt++;
+        }
+        if (cnt < k) {
+            return null;
+        }
+        return p ;
+    }
+
+    private static LinkNode create() {
+    }
 }
